134c2d90b43a346fcbe53f585395a35cdf5b0a0a
awoelf <98348782+awoelf@users.noreply.github.com>
2022-10-10 14:20:19 -0500

    converting recipe card html to jquery

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,10 @@
 API_KEY = `fdd93af3e93f49288bba2c709b2f9f6a`; // HIDE THIS LATER
 RECIPE_URL = `https://api.spoonacular.com/recipes/complexSearch?&apiKey=${API_KEY}`;
 
-fetch(RECIPE_URL, {
-    method: 'GET'
-})
-    .then(function (response) {
-        return response.json();
-    })
-    .then (function (data) {
-        console.log(data);
-    })
 
-let recipeCard =
+
+function printRecipeCards(resultObj) {
+    let recipeCard =
     `<div class="recipe-card card my-5">
         <div class="columns">
           <div class="column is-one-third is-paddingless">
@@ -33,44 +26,111 @@ let recipeCard =
                 class="column is-flex is-flex-direction-column is-justify-content-space-around is-narrow"
               >
                 <div class="card-item">
-                  <span class="material-symbols-outlined icon">timer</span>
-                  <p id="time">45 mins</p>
+                  <span class="material-symbols-outlined icon">
+                  </span>
+                  <p id="time"></p>
                 </div>
                 <div class="card-item">
-                  <span class="material-symbols-outlined icon">bolt</span>
-                  <p id="calories">250 cal/serving</p>
+                  <span class="material-symbols-outlined icon">
+                  </span>
+                  <p id="calories"></p>
                 </div>
                 <div class="card-item">
-                  <span class="material-symbols-outlined icon">egg</span>
-                  <p id="ingredients">5 ingredients</p>
+                  <span class="material-symbols-outlined icon">
+                  </span>
+                  <p id="ingredients"></p>
                 </div>
               </div>
               <div
                 class="column is-flex is-flex-direction-column is-justify-content-space-around"
               >
                 <div class="card-item">
-                  <span class="material-symbols-outlined icon"
-                    >restaurant_menu</span
-                  >
-                  <p id="difficulty">Easy</p>
+                  <span class="material-symbols-outlined icon">
+                  </span>
+                  <p id="difficulty"></p>
                 </div>
                 <div class="card-item">
-                  <span class="material-symbols-outlined icon">set_meal</span>
-                  <p id="diet">Vegetarian</p>
+                  <span class="material-symbols-outlined icon">
+                  </span>
+                  <p id="diet"></p>
                 </div>
                 <div class="card-item">
-                  <span class="material-symbols-outlined icon"
-                    >shopping_cart</span
-                  >
-                  <p id="price">$10</p>
+                  <span class="material-symbols-outlined icon">
+                  </span>
+                  <p id="price">Price</p>
                 </div>
               </div>
             </div>
           </div>
         </div>`;
-
-array.forEach(element => {
     
-});
-$("#recipe-results").append(recipeCard);
+}
+
+
+
+
+fetch(RECIPE_URL, {
+    method: 'GET'
+})
+    .then(function (response) {
+        return response.json();
+    })
+    .then (function (data) {
+        console.log(data);
+    })
+
+
+let recipeResult = $("<div>").addClass("recipe-card card my-5");
+let cardColumns = $("<div>").addClass("columns");
+recipeResult.append(cardColumns);
+let image = $("<div>").addClass("column is-one-third is-paddingless")
+    .append($("<img>").attr("id", "recipe-img")
+    .addClass("card-image")
+    .attr("src", "./assets/images/card-placeholder.jpg"))
+cardColumns.append(image);
+let cardContent = $("<div>")
+    .addClass("column card-content is-flex is-flex-direction-column is-align-items-stretch m-4")
+let recipeName = $("<h3>").addClass("is-size-3 mt-1 pb-2").text("Recipe name");
+cardContent.append(recipeName);
+let itemColumns = $("<div>").addClass("columns mt-2 item-box mb-2");
+let items = $("<div>")
+    .addClass("column is-flex is-flex-direction-column is-justify-content-space-around is-narrow");
+itemColumns.append(items
+    .append(
+        $("<div>").addClass("card-item")
+        .append($("<span>").addClass("material-symbols-outlined").text("timer"))
+        .append($("<p>").attr("id", "time"))
+    )
+    .append(
+        $("<div>").addClass("card-item")
+        .append($("<span>").addClass("material-symbols-outlined").text("bolt"))
+        .append($("<p>").attr("id", "calories"))
+    )
+    .append(
+        $("<div>").addClass("card-item")
+        .append($("<span>").addClass("material-symbols-outlined").text("egg"))
+        .append($("<p>").attr("id", "ingredients"))
+    ));
+itemColumns.append(items
+    .append(
+        $("<div>").addClass("card-item")
+        .append($("<span>").addClass("material-symbols-outlined").text("restaurant_menu"))
+        .append($("<p>").attr("id", "difficulty"))
+    )
+    .append(
+        $("<div>").addClass("card-item")
+        .append($("<span>").addClass("material-symbols-outlined").text("set_meal"))
+        .append($("<p>").attr("id", "diet"))
+    )
+    .append(
+        $("<div>").addClass("card-item")
+        .append($("<span>").addClass("material-symbols-outlined").text("shopping_cart"))
+        .append($("<p>").attr("id", "price"))
+    ));
+
+
+
+cardContent.append(itemColumns);
+cardColumns.append(cardContent);
+$("#recipe-results").append(recipeResult);
 
