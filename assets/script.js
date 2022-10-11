9005035e3e32fb50dd9843b8263e6d5a1007ce25
L-Jones-hub <105738902+L-Jones-hub@users.noreply.github.com>
2022-10-11 12:55:47 -0500

    Working on responsiveness

--- assets/script.js
+++ assets/script.js
@@ -20,7 +20,7 @@ function searchApi(query, category) {
     .then(function (data) {
       if (!data.results.length) {
         console.log("No results.");
-        ($("#recipe-results")).text("No results.")
+        $("#recipe-results").text("No results.");
       } else {
         for (let i = 0; i < data.results.length; i++) {
           createCard(data.results[i]);
@@ -29,8 +29,7 @@ function searchApi(query, category) {
     });
 }
 
-function cardDetails(recipeId)
-{
+function cardDetails(recipeId) {
   let apiKey = `a696c5ce649c4449b5b6afa1f5454ca9`; // HIDE THIS LATER
   let detailsUrl = `https://api.spoonacular.com/recipes/${recipeId}/information?&apiKey=${apiKey}&includeNutrition=true`;
   fetch(detailsUrl, {
@@ -44,33 +43,37 @@ function cardDetails(recipeId)
     })
     .then(function (data) {
       $(`#${recipeId}`).find("#time").text(`${data.readyInMinutes} min`);
-      $(`#${recipeId}`).find("#calories").text(`${data.nutrition.nutrients[0].amount} calories`);
-      $(`#${recipeId}`).find("#ingredients").text(`${data.extendedIngredients.length()} ingredients`);
-      console.log()
+      $(`#${recipeId}`)
+        .find("#calories")
+        .text(`${data.nutrition.nutrients[0].amount} calories`);
+      $(`#${recipeId}`)
+        .find("#ingredients")
+        .text(`${data.extendedIngredients.length()} ingredients`);
+      console.log();
       $(`#${recipeId}`).find("#servings").text(`${data.servings} servings`);
       $(`#${recipeId}`).find("#diet").text(`${data.diets[0]} ingredients`);
       $(`#${recipeId}`).find("#price").text(`${data.pricePerServing}/serving`);
     });
-  }
+}
 
 function createCard(resultObj) {
   console.log(resultObj);
-  let recipeResult = $("<div>").addClass("recipe-card card my-5").attr("id", resultObj.id);
+  let recipeResult = $("<div>")
+    .addClass("recipe-card card my-5")
+    .attr("id", resultObj.id);
   let cardColumns = $("<div>").addClass("columns");
   recipeResult.append(cardColumns);
   let image = $("<div>")
     .addClass("column is-one-third is-paddingless")
-    .append(
-      $("<img>")
-        .attr("id", "recipe-img")
-        .addClass("card-image")
-    );
+    .append($("<img>").attr("id", "recipe-img").addClass("card-image"));
   cardColumns.append(image);
   let cardContent = $("<div>")
     .addClass(
       "column card-content is-flex is-flex-direction-column is-align-items-stretch m-4"
     )
-    .append($("<h3>").addClass("is-size-3 mt-1 pb-2").attr("id", "recipe-name"));
+    .append(
+      $("<h3>").addClass("is-size-3 mt-1 pb-2").attr("id", "recipe-name")
+    );
   let itemColumns = $("<div>").addClass("columns mt-2 item-box mb-2");
   itemColumns.append(
     $("<div>")
@@ -140,6 +143,6 @@ function createCard(resultObj) {
   $(`#${resultObj.id}`).find("#recipe-name").text(`${resultObj.title}`);
   $(`#${resultObj.id}`).find("#recipe-img").attr("src", resultObj.image);
   cardDetails(resultObj.id);
-};
+}
 
 searchApi("pizza");
